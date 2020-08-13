4d464fdd0a9395b6ff4a2542554d2c4775a8328e
RSN <rsn3230@gmail.com>
2020-08-13 15:07:57 +0530

    commit code for add one or multiple filels

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
 
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+
+import {
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  UploadedFiles,
+  Res,
+  Param,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName, imageFileFilter } from './utils/file-uploading.utils';
+import { AppService } from './app.service';
+ 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +34,51 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadedFile(@UploadedFile() file) {
+    const response = {
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+    return response;
+  }
+
+  @Post('multiple')
+  @UseInterceptors(
+    FilesInterceptor('image', 20, {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadMultipleFiles(@UploadedFiles() files) {
+    const response = [];
+    files.forEach(file => {
+      const fileReponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileReponse);
+    });
+    return response; 
+  }
+
+  @Get(':imgpath')
+  seeUploadedFile(@Param('imgpath') image, @Res() res) {
+    return res.sendFile(image, { root: './files' });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 // async function bootstrap() {
 //   const app = await NestFactory.create(AppModule);
@@ -8,7 +9,14 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
   const app = await NestFactory.create(AppModule);
+  // app.useStaticAssets(`${__dirname}/public`);
+  // the next two lines did the trick
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.enableCors();
   await app.listen(3000);
 
   if (module.hot) {
